ac0b4d9524005c09213fcf29705a513b14408035
esudreame <EvgeniyHolovko@pdpu.edu.ua>
2024-12-23 22:34:02 +0200

    Adding: - NotBlank validation for SingInRequest

--- src/main/java/com/lumiring/updateManager/domain/api/user/auth/SingInRequest.java
+++ src/main/java/com/lumiring/updateManager/domain/api/user/auth/SingInRequest.java
@@ -1,8 +1,6 @@
 package com.lumiring.updateManager.domain.api.user.auth;
 
-
-
-
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,16 +12,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SingInRequest {
 
-
-//    @Size(min = 5, max = 255, message = "login должен содержать от 5 до 255 символов")
-//    @Schema(description = "Адрес электронной почты или имя пользователя", example = "jondoe@gmail.com или John")
-//    @NotBlank(message = "login должен быть заполнен")
+    @NotBlank(message = "login должен быть заполнен")
     private String login;
 
-//    @Schema(description = "Пароль", example = "my_1secret1_password")
-//    @Size(max = 255, message = "Длина пароля должна быть не более 255 символов")
-//    @NotBlank(message = "password должен быть заполнен")
-//    @Pattern(regexp = RegExp.password, message = "incorrect password")
+    @NotBlank(message = "password должен быть заполнен")
     private String password;
 
 }
